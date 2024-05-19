666b2ccf9270362bf8bf57c0802114ec4f90e5e0
KorotunM <144013223+KorotunM@users.noreply.github.com>
2024-05-19 20:23:12 +0300

    create task1

--- src/Task.java
+++ src/Task.java
@@ -10,6 +10,16 @@ public class Task {
         return Math.round(randomNumber * znak) / znak;
     }
     public String answer() {return "";}
+//    public int gcd(int a, int b) {
+//        if (a == 0 && b == 0)
+//            return 1;
+//        if (a == 0 || b == 0)
+//            return (a == 0) ? b : a;
+//        int remainder = a % b;
+//        if (remainder == 0) {
+//            return b;
+//        } else return gcd(b, remainder);
+//    }
 
 
 }
--- src/Task1.java
+++ src/Task1.java
@@ -4,31 +4,44 @@ public class Task1 extends Task {
     int count1 = randomize(2, 4);
     int count2 = randomize(6, 8);
     int photos1 = randomize(10, 13), photos2 = randomize(3, 7);
+    int variant = randomize(5, 6);
 
     public String fill() {
-            if (randomize(5, 6) == 5) {
-                return  "1. В конверте " + photos1 + " фотографий, на двух из которых изображены отец и сын, объявленные в розыск. Следователь извлекает наугад последовательно без возвращения " +  photos2 + " фотографий. Найти вероятность того, что:\n" +
-                        "а) на первой из извлеченных фотографии будет отец, а на второй — сын;\n" +
-                        "б) фотография отца попадется раньше, чем фотография сына.\n";
-            } else {
-                return "1. " + variants + " вариантов контрольной работы по математике\n" +
+        if (variant == 5) {
+            return "1. В конверте " + photos1 + " фотографий, на двух из которых изображены отец и сын, объявленные в розыск. Следователь извлекает наугад последовательно без возвращения " + photos2 + " фотографий. Найти вероятность того, что:\n" +
+                    "а) на первой из извлеченных фотографии будет отец, а на второй — сын;\n" +
+                    "б) фотография отца попадется раньше, чем фотография сына.\n";
+        } else {
+            return "1. " + variants + " вариантов контрольной работы по математике\n" +
                     " распределяются случайным образом среди " + students + "\n" +
                     " студентов, сидящих в одном ряду. Каждый получает по\n" +
                     "одному варианту. Найти вероятность того, что:\n" +
                     "а) варианты 1-й и 2-й достанутся первым " + count1 + " студентам;\n" +
                     "б) первые " + count2 + " вариантов распределятся последовательно.";
-            }
+        }
     }
+
     public String answer() {
-        int m = 1;
-        for (int i = 2; i < photos2; i++) {
-            m *= photos1 - i;
+        if (variant == 5) {
+            int m = 1;
+            for (int i = 2; i < photos2; i++) {
+                m *= photos1 - i;
+            }
+            int znam = 1;
+            for (int i = 0; i < (photos1 - photos2); i++) {
+                znam *= photos1 - i;
+            }
+            int ob = gcd(m, znam);
+            return m/ob + "/" + znam/ob + "C";
+        } else {
+            int numerator = 1;
+            for (int i = 0; i < students; i++)
+                numerator *= students - i;
+            int znam = 1;
+            for (int i = 0; i < variants; i++)
+                znam *= variants - i;
+            int ob = gcd(numerator, znam);
+            return "a) "+ numerator/ob +"/"+znam/ob+"M";
         }
-        int znam = 1;
-        for (int i = 0; i < (photos1-photos2); i++) {
-            znam *= photos1 - i;
-        }
-        
-        return m + "/" + znam;
     }
 }
